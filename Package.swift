e9b990012027c9b4fed9e3955e972ceaecd2a780
Nocturnal <contact@nocturnal.solutions>
2018-06-20 19:01:15 -0600

    Use my bug-fixed branch of Kitura-net.

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.4.0"),
         .package(url: "https://github.com/NocturnalSolutions/Swift-Kuery.git", .branch("escape-apostrophes")),
+        .package(url: "https://github.com/NocturnalSolutions/Kitura-net.git", .branch("ClientRequest-Redirection")),
         .package(url: "https://github.com/IBM-Swift/Swift-Kuery-SQLite.git", from: "1.0.0"),
         .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.9.1"),
         .package(url: "https://github.com/IBM-Swift/Configuration.git", from: "3.0.1"),
@@ -18,12 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "WastebinApp",
-            dependencies: ["Kitura", "SwiftKuerySQLite", "KituraStencil", "Configuration"]
-        ),
-        .target(
             name: "Wastebin",
             dependencies: ["WastebinApp"]
+        ),
+        .target(
+            name: "WastebinApp",
+            dependencies: ["Kitura", "SwiftKuerySQLite", "KituraStencil", "Configuration"]
         ),
         .testTarget(
             name: "WastebinTests",
